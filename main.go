7432a37e94a45a5ed9d65a6d88193e92bb618fb6
Sean E. Russell <ser@ser1.net>
2020-03-04 09:49:10 -0600

    Adds Double and Triple functions

--- main.go
+++ main.go
@@ -6,13 +6,21 @@ import (
 	"plugin"
 )
 
+func Double(a int) int {
+	return 2 * a
+}
+
+func Triple(a int) int {
+	return 3 * a
+}
+
 func main() {
 	flag.Parse()
 	p, err := plugin.Open(flag.Arg(0))
 	if err != nil {
 		panic(err)
 	}
-	fp, err := p.Lookup("Add")
+	fp, err := p.Lookup("AddDouble")
 	if err != nil {
 		panic(err)
 	}
